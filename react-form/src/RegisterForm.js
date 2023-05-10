9b04998e36dc4e55967759214f7d53298b19eda0
mayara-morais <57415644+mayara-morais@users.noreply.github.com>
2023-05-10 19:00:20 -0300

    Feat: react native project setup

---
+++ react-form/src/RegisterForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const RegisterForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission
+    console.log('email:', email);
+    console.log('password:', password);
+    console.log('confirmPassword:', confirmPassword);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
---
+++ react-form/src/RegisterScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from './RegisterForm';
+
+const RegisterScreen = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterScreen;
